19f4d182e78e8176b1f43032729720241e73d8b2
Reece White <brplatinum@gmail.com>
2021-02-04 15:46:41 +1100

    Added Device Tab button and Device Type dropdown box

--- src/main/java/Device.java
+++ src/main/java/Device.java
@@ -6,4 +6,19 @@ public class Device {
         deviceType = null;
         path = null;
     }
+
+    public void setDeviceType(String deviceTypeInput){
+        deviceType = deviceTypeConvert(deviceTypeInput);
+    }
+
+    private DeviceType deviceTypeConvert(String deviceType) {
+        switch (deviceType){
+            case "Kindle":
+                return DeviceType.KINDLE;
+            case "Kobo":
+                return DeviceType.KOBO;
+            default:
+                return null;
+        }
+    }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,17 +1,53 @@
 import javafx.application.Application;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
+    Button btnDeviceInfo;
+    ComboBox cmbDeviceType;
+    Device device;
+
     @Override
     public void start(Stage stage) {
-        String javaVersion = System.getProperty("java.version");
-        String javafxVersion = System.getProperty("javafx.version");
-        Label l = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
-        Scene scene = new Scene(new StackPane(l), 640, 480);
+        device = new Device();
+
+        GridPane grid = new GridPane();
+
+        stage.setTitle("Ereader Highlights Sync");
+
+        btnDeviceInfo = new Button();
+        btnDeviceInfo.setText("Device");
+        grid.add(btnDeviceInfo, 1, 0);
+
+        ObservableList<String> deviceTypeOptions = FXCollections.observableArrayList(
+                "Kindle",
+                "Kobo"
+        );
+
+        cmbDeviceType = new ComboBox();
+        cmbDeviceType.setItems(deviceTypeOptions);
+        cmbDeviceType.valueProperty().addListener(new ChangeListener() {
+            @Override
+            public void changed(ObservableValue observableValue, Object o, Object t1) {
+                device.setDeviceType((String) t1);
+            }
+        });
+
+        grid.add(cmbDeviceType, 2, 0);
+
+        Scene scene = new Scene(grid, 300, 250);
         stage.setScene(scene);
         stage.show();
     }
@@ -19,5 +55,4 @@ public class Main extends Application {
     public static void main(String[] args) {
         launch();
     }
-
 }
