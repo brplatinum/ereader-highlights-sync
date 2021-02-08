b1e59043a05c7c267b95bb311b6929a97f429f17
Reece White <brplatinum@gmail.com>
2021-02-08 18:38:04 +1100

    Now static method for removing special characters

--- src/main/java/org/brplatinum/TextHelper.java
+++ src/main/java/org/brplatinum/TextHelper.java
@@ -1,16 +1,26 @@
 package org.brplatinum;
 
-public class CSVHelper {
+public class TextHelper {
+    //Parallel arrays for removing special characters
+    private static final String[] specialCharacters = {"\u2018|\u2019", "\u201C|\u201D", "\u2026"};
+    private static final String[] specialCharacterReplacements = {"'", "\"", "..."};
+
     public static String csvFix(String input) { //Fixes formatting for CSV and converts some special unicode characters to standard characters
         String fixedInput = input.replaceAll("\\r\\n", " ");
         fixedInput = fixedInput.replaceAll("\\r", " ");
         fixedInput = fixedInput.replaceAll("\\n", " ");
-        fixedInput = fixedInput.replaceAll("\u2018|\u2019", "'");
-        fixedInput = fixedInput.replaceAll("\u201C|\u201D", "\"");
-        fixedInput = fixedInput.replaceAll("\u2026", "...");
+        fixedInput = removeSpecialCharacters(fixedInput);
         if (fixedInput.contains(",") || fixedInput.contains("\"") || fixedInput.contains("'")) {
             fixedInput = fixedInput.replaceAll("\"", "\"\"");
             fixedInput = "\"" + fixedInput + "\"";
+        }
+        return fixedInput;
+    }
+
+    public static String removeSpecialCharacters(String input) {
+        String fixedInput = input;
+        for (int i = 0; i < specialCharacters.length; i++) {
+            fixedInput = fixedInput.replaceAll(specialCharacters[i], specialCharacterReplacements[i]);
         }
         return fixedInput;
     }
