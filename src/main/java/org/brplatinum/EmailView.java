ec04fa4784dc29999f35bdeb991bb835f123584b
Reece White <brplatinum@gmail.com>
2021-02-09 15:59:34 +1100

    Added ability to change Email server settings for sending email

--- src/main/java/org/brplatinum/EmailView.java
+++ src/main/java/org/brplatinum/EmailView.java
@@ -2,9 +2,7 @@ package org.brplatinum;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.layout.VBox;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
@@ -17,18 +15,35 @@ public class EmailView {
         window.initModality(Modality.APPLICATION_MODAL);
         window.setMinWidth(400);
 
+        TextField txtHostname = new TextField();
         TextField txtUserEmail = new TextField();
-        TextField txtUserPassword = new TextField();
+        PasswordField pwfUserPassword = new PasswordField();
         TextField txtDestinationEmail = new TextField();
+        Spinner spnPort = new Spinner(1, Integer.MAX_VALUE, 1);
+        spnPort.setEditable(true);
 
+        ToggleGroup tggEncryptionTypes = new ToggleGroup();
+
+        RadioButton rdbTLS = new RadioButton("TLS");
+        rdbTLS.setToggleGroup(tggEncryptionTypes);
+        rdbTLS.setSelected(true);
+        RadioButton rdbSSL = new RadioButton("SSL");
+        rdbSSL.setToggleGroup(tggEncryptionTypes);
+        RadioButton rdbNone = new RadioButton("None");
+        rdbNone.setToggleGroup(tggEncryptionTypes);
+
+        Label lblHostName = new Label("Hostname:");
         Label lblUserEmail = new Label("Your Email:");
         Label lblUserPassword = new Label("Your Password:");
         Label lblDestinationEmail = new Label("Destination Email:");
+        Label lblPort = new Label("Port:");
+        Label lblEncryption = new Label("Encryption");
+
 
         Button btnSendEmail = new Button("Send");
 
         VBox layout = new VBox(10);
-        layout.getChildren().addAll(lblUserEmail, txtUserEmail, lblUserPassword, txtUserPassword, lblDestinationEmail, txtDestinationEmail, btnSendEmail);
+        layout.getChildren().addAll(lblUserEmail, txtUserEmail, lblUserPassword, pwfUserPassword, lblHostName, txtHostname, lblPort, spnPort, lblEncryption, rdbTLS, rdbSSL, rdbNone, lblDestinationEmail, txtDestinationEmail, btnSendEmail);
 
         Scene scene = new Scene(layout);
         window.setScene(scene);
